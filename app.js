e999d60c097f4bd38b7b8720ad4ef4335493ce47
rahulgope45 <rahuleile456@gmail.com>
2025-05-04 22:06:38 +0530

    Conversion is comming right

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
-// const Base_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/inr.json"
-// const URL_Img = "https://flagsapi.com/IN/flat/64.png"
-// const dropdowns = document.querySelectorAll(".dropdown select")
-// const Fromwala_select = document.querySelector(".Fromwala select")
-// const fromimg = document.querySelector(".Fromwala img")
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies"
 
-
-
-// Fromwala_select.addEventListener("change" ,()=>{
-
-//     fromimg.src = `https://flagsapi.com/${countryList[Fromwala_select.value]}/flat/64.png`
-
-// })
-
-
-
-
-const URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/inr.json"
 
 const dropdowns = document.querySelectorAll(".dropdown select")
 const Fromwala_select = document.querySelector(".Fromwala select")
@@ -23,6 +7,9 @@ const fromimg = document.querySelector(".Fromwala img")
 const Towala_select = document.querySelector(".Towala select")
 const toimg = document.querySelector(".Towala img")
 
+const amounts =document.querySelector(".amount input")
+const resultmsg = document.querySelector(".msg #msg")
+const convertButton = document.querySelector(".button button")
 
 
 
@@ -59,6 +46,48 @@ Towala_select.addEventListener("change" ,() =>{
     toimg.src = `https://flagsapi.com/${countryList[Towala_select.value]}/flat/64.png`
 
 })
+
+
+//adding Convert Function
+convertButton.addEventListener("click" , async(e) =>{
+    e.preventDefault()
+
+    const amount = parseFloat(amounts.value)
+    const fromcurr = Fromwala_select.value.toLowerCase()
+    const tocurr = Towala_select.value.toLowerCase()
+
+
+
+
+    if(isNaN (amount) || amount<= 0){
+        resultmsg.innerText = "Please enter valid amount"
+        return
+    }
+    
+        try{
+            const response = await fetch(`${BASE_URL}/${fromcurr}.json`)
+        const data = await response.json()
+        const rate = data[fromcurr][tocurr]
+
+        const result = (amount * rate).toFixed(2)
+
+        resultmsg.innerText = `${amount} ${Fromwala_select.value} = ${result} ${Towala_select.value}`
+
+        }catch(error)
+        {
+            resultmsg.innerText = "Failed to fetch exchange rate. Please try again.";
+        console.error("Error fetching exchange rate:", error);
+
+        }
+    
+        
+
+
+    
+})
+
+
+
 
 
 
